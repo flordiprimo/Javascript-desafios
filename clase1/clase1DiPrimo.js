59a82404f00d5b6cf3bee00eaed3d76bf103c115
flordiprimo <flordiprimo@gmail.com>
2021-10-30 15:55:00 -0300

    desafio clase 1

---
+++ clase1/clase1DiPrimo.js
@@ -0,0 +1,11 @@
+let nombre = prompt("Ingresá tu nombre");
+
+console.log("Usuario: " + nombre);
+
+let numeroA = prompt("Elegí un número");
+let numeroB = prompt("Elegí otro número");
+
+let suma = parseInt(numeroA) + parseInt(numeroB);
+
+alert( nombre + ", la suma de tus números es: " + suma);
+console.log("La suma de " + nombre + " es: " + suma);
